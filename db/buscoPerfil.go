2751131a92eb4f93b5de177dde406674d8f0b91a
Donad Joao <dedojo-2728@outlook.com>
2021-05-15 13:24:34 -0600

    Busqueda de perfil

---
+++ db/buscoPerfil.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/DonaldJoao/twittor/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// BuscoPerfil busca un perfil en la BD
+func BuscoPerfil(ID string) (models.Usuario, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	db := MongoCN.Database("twittor")
+	col := db.Collection("usuarios")
+
+	var perfil models.Usuario
+	objID, _ := primitive.ObjectIDFromHex(ID)
+
+	condicion := bson.M{
+		"_id": objID,
+	}
+
+	err := col.FindOne(ctx, condicion).Decode(&perfil)
+	perfil.Password = ""
+
+	if err != nil {
+		fmt.Println("Registro no encontado " + err.Error())
+		return perfil, err
+	}
+
+	return perfil, err
+}
---
+++ middlewares/validoJWT.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/DonaldJoao/twittor/routers"
+)
+
+// ValidoJWT permite validar el JWT que nos viene en la petcion
+func ValidoJWT(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, _, _, err := routers.ProcesoToken(r.Header.Get("Authorization"))
+		if err != nil {
+			http.Error(w, "Error en el Token ! "+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
