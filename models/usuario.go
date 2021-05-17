6a8a4c6411df6fbdf083f98f840f9ba7297ff9b5
Donad Joao <dedojo-2728@outlook.com>
2021-05-17 07:36:39 -0600

    ruta ver perfil

--- models/usuario.go
+++ models/usuario.go
@@ -8,14 +8,14 @@ import (
 
 /* Usuario es el modelo de usuario de la base de datos */
 type Usuario struct {
-	ID               primitive.ObjectID `bson:"_id,omitempy" json:"id"`
-	Nombre           string             `bson:"nombre" json:"nombre,omitempy"`
-	Apellidos        string             `bson:"apellidos" json:"apellidos,omitempy"`
-	FechaNacimientos time.Time          `bson:"fechaNacimientos" json:"fechaNacimientos,omitempy"`
+	ID               primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Nombre           string             `bson:"nombre" json:"nombre,omitempty"`
+	Apellidos        string             `bson:"apellidos" json:"apellidos,omitempty"`
+	FechaNacimientos time.Time          `bson:"fechaNacimientos" json:"fechaNacimientos,omitempty"`
 	Email            string             `bson:"email" json:"email"`
-	Password         string             `bson:"password" json:"password,omitempy"`
-	Avatar           string             `bson:"avatar" json:"avatar,omitempy"`
-	Banner           string             `bson:"banner" json:"banner,omitempy"`
-	Biografia        string             `bson:"biografia" json:"biografia,omitempy"`
-	SitioWeb         string             `bson:"sitioWeb" json:"sitioWeb,omitempy"`
+	Password         string             `bson:"password" json:"password,omitempty"`
+	Avatar           string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner           string             `bson:"banner" json:"banner,omitempty"`
+	Biografia        string             `bson:"biografia" json:"biografia,omitempty"`
+	SitioWeb         string             `bson:"sitioWeb" json:"sitioWeb,omitempty"`
 }
